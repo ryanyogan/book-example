8d63e762e39ef43dacda5e0ca33aa980b283511d
Ryan Yogan <ryanyogan@gmail.com>
2014-03-18 19:22:56 -0500

    Redirect after post and show all items

--- functional_tests.py
+++ functional_tests.py
@@ -27,7 +27,10 @@ class NewVisitorTest(unittest.TestCase):
                 'Enter a to-do item'
                 )
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
