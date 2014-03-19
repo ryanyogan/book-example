b856eb9ea63ac567130aed6b6db8c0d7cbad9817
Ryan Yogan <ryanyogan@gmail.com>
2014-03-18 22:32:22 -0500

    new url, view and template to displays lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+      name='view_list'
+      ),
 )
